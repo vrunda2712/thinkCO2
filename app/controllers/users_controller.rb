d51361e4cc7f06c59b956d37c8861131d008c5c7
github.com/Ash3616 <aishwaryam.pandya@hotmail.com>
2018-03-06 11:52:33 -0500

    landing page up

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,4 @@
+class UsersController < ApplicationController
+  def index # find index file --> views 
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+ root 'users#index' # default route to landing page
+
+ resources :users # will allow to access all resources of users
+ resources :calculator, only: %i(index create) # calculator has only access to index resource files
+ resources :sessions, only: %i(new create destroy) # will allow to access all resources of sessions
+ resources :scores # will allow to access all resources of scores
 end
